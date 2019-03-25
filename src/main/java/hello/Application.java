02f87b5243adc7fac69b1c5007e862c3e02276a2
rafaelortegamendez <rafael.ortega.mendez@alumnos.upm.es>
2019-03-25 11:26:20 +0100

    cambiosxSA

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,8 @@ System.out.println("RAUL EL CICLISTA");
 
 System.out.println("Diego vente ya. Vamos");
 System.out.println("Rfa no se entera ni del clima");	
-
+System.out.println("oie loco que dices");	
+System.out.println("oie muchachos oisteis ese rempalago");
 }
 
 
